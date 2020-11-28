c57e5613d19dd645e352243965490b4b025b14e3
Jiayi Hao <73452111+jh701@users.noreply.github.com>
2020-11-27 19:45:17 -0500

    Winter is Coming: Press ‘1’ for SNOW STORM

--- Homework Week 05/JiayiHao_Week5_WinterIsComing/src/ofApp.cpp
+++ Homework Week 05/JiayiHao_Week5_WinterIsComing/src/ofApp.cpp
@@ -79,9 +79,9 @@ void ofApp::update(){
 
 //--------------------------------------------------------------
 void ofApp::draw(){
-        ofDrawBitmapString("Winter is Coming: Press ‘1’ for SNOW STORM", 10 , 20);
-        
         ofBackgroundGradient(ofColor(80, 70, 70), ofColor(0, 0, 0), OF_GRADIENT_CIRCULAR);
+        ofDrawBitmapString("Winter is Coming: Press ‘1’ for SNOW STORM", 10 , 20);
+        ofDrawBitmapString("EasyCam installed", 10 , 20);
 
         Cam.begin();
                ofPushMatrix();
